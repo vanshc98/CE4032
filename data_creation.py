f58925853992df398c7662a17bc9468c23acb93c
Gerald <geraldjktan95@gmail.com>
2019-10-23 12:14:17 +0800

    fix changes

--- data_creation.py
+++ data_creation.py
@@ -8,16 +8,25 @@ from utils import calHarDist, one_hot
 
 local_tz = pytz.timezone('Portugal')
 test_data = pd.read_csv('datasets/test.csv')
-
 train_data = pd.read_csv('datasets/train.csv')
-train_data.head()
-train_data.groupby('MISSING_DATA').count()
+
+#train_data.head()
+#train_data.groupby('MISSING_DATA').count()
 
 holiday_data = pd.read_csv('datasets/Holidays.csv')
-holiday_data.head()
+#holiday_data.head()
 
-datestamp_list = []
+print("Removing Missing Data")
+#only exist missing data in train data
+for index, row in train_data.iterrows():
+    if(row['MISSING_DATA']== True):
+        train_data.drop(index, inplace = True)
+
+train_data = train_data.reset_index(drop=True)
+
 print("Adding date")
+#add datestamp to test data
+datestamp_list = []
 
 for index, row in test_data.iterrows():
     epoch_datestamp = row['TIMESTAMP']
@@ -44,6 +53,7 @@ for index, row in holiday_data.iterrows():
         holiday_datestamp = holiday_datestamp.replace("\t", "")
         holiday_datetime = datetime.strptime(holiday_datestamp, '%d.%m.%Y')
         holiday_date.append(holiday_datetime)
+
 holiday_data['DateStamp']=holiday_date
 
 print("Adding actual day type")
@@ -106,72 +116,91 @@ train_data.groupby('ACTUAL_DAYTYPE').count()
 print("Adding duration")
 
 #for test data
-counter = 0
-count_md = test_data['MISSING_DATA'].count()
-trip_duration = []
-end_time = []
-origin = []
-dest = []
-for i in range(count_md):
-    polyline = ast.literal_eval(test_data['POLYLINE'][i])
-    dur = len(polyline)
-    trip_duration.append(dur)
-    end_epoch_datestamp = test_data['TIMESTAMP'][i] + dur
-    end_datetime_for_datestamp_in_utc = datetime.utcfromtimestamp(end_epoch_datestamp)
-    actual_end_datetime_for_datestamp = end_datetime_for_datestamp_in_utc.replace(tzinfo=pytz.utc).astimezone(local_tz)
-    end_time.append(actual_end_datetime_for_datestamp)
-    poly_arr = polyline
-    if(poly_arr==[]):
-        origin.append("NULL")
-        dest.append("NULL")
-    else:
-        origin.append(poly_arr[0])
-        dest.append(poly_arr[-1])
-    counter+=1
-    if(counter%500==0):
-        print(counter/1710670)
-test_data['DURATION'] = trip_duration
-test_data['END_TIME'] = end_time
-test_data['ORIGIN'] = origin
-test_data['DESTINATION'] = dest
 
-#for train data
-counter = 0
-count_md = train_data['MISSING_DATA'].count()
 trip_duration = []
 end_time = []
 origin = []
 dest = []
-for i in range(count_md):
+
+for i in range(test_data.shape[0]):
     try:
-        polyline = ast.literal_eval(train_data['POLYLINE'][i])
-        dur = len(polyline)
-        trip_duration.append(dur)
-        end_epoch_datestamp = train_data['TIMESTAMP'][i] + dur
-        end_datetime_for_datestamp_in_utc = datetime.utcfromtimestamp(end_epoch_datestamp)
-        actual_end_datetime_for_datestamp = end_datetime_for_datestamp_in_utc.replace(tzinfo=pytz.utc).astimezone(local_tz)
-        end_time.append(actual_end_datetime_for_datestamp)
-        poly_arr = polyline
-        if(poly_arr==[]):
+        polyline = test_data['POLYLINE'][i]
+        if(polyline==[]):
             origin.append("NULL")
             dest.append("NULL")
+            trip_duration.append("NULL")
+            end_time.append("NULL")
         else:
-            origin.append(poly_arr[0])
-            dest.append(poly_arr[-1])
+            poly_arr = polyline.split('],[')
+            dur = len(poly_arr)
+            trip_duration.append(dur)
+            endtime = train_data['TIMESTAMP'][i] + (dur * 15)
+            end_time.append(endtime)
+            origin.append(poly_arr[0][2:])
+            dest.append(poly_arr[-1][:-2])
     except:
-        trip_duration.append("NULL")
-        end_time.append("NULL")
         origin.append("NULL")
         dest.append("NULL")
-    counter+=1
-    if(counter%500==0):
-        print(counter/1710670)
+        trip_duration.append("NULL")
+        end_time.append("NULL")
+    #if (i%100000) == 0:
+    #    print(i)
+
+test_data['DURATION'] = trip_duration
+test_data['ORIGIN'] = origin
+test_data['DESTINATION'] = dest
+test_data['END_TIME'] = end_time
+
+test_data = test_data.dropna(subset=['ORIGIN'])
+test_data = test_data.reset_index(drop=True)
+
+test_data['ORIGIN'] = test_data['ORIGIN'].str.replace(r']', '')
+test_data['DESTINATION'] = test_data['DESTINATION'].str.replace(r'[', '')
+
+#for train data
+
+trip_duration = []
+end_time = []
+origin = []
+dest = []
+
+for i in range(train_data.shape[0]):
+    try:
+        polyline = train_data['POLYLINE'][i]
+        if(polyline==[]):
+            origin.append("NULL")
+            dest.append("NULL")
+            trip_duration.append("NULL")
+            end_time.append("NULL")
+        else:
+            poly_arr = polyline.split('],[')
+            dur = len(poly_arr)
+            trip_duration.append(dur)
+            endtime = train_data['TIMESTAMP'][i] + (dur * 15)
+            end_time.append(endtime)
+            origin.append(poly_arr[0][2:])
+            dest.append(poly_arr[-1][:-2])
+    except:
+        origin.append("NULL")
+        dest.append("NULL")
+        trip_duration.append("NULL")
+        end_time.append("NULL")
+    #if (i%100000) == 0:
+    #    print(i)
+
 train_data['DURATION'] = trip_duration
-train_data['END_TIME'] = end_time
 train_data['ORIGIN'] = origin
 train_data['DESTINATION'] = dest
+train_data['END_TIME'] = end_time
 
-# get day of week and hour 
+train_data = train_data.dropna(subset=['ORIGIN'])
+train_data = train_data.reset_index(drop=True)
+
+train_data['ORIGIN'] = train_data['ORIGIN'].str.replace(r']', '')
+train_data['DESTINATION'] = train_data['DESTINATION'].str.replace(r'[', '')
+
+
+# get day of week and hour
 
 print("Adding day of week and hour")
 
@@ -220,15 +249,15 @@ for i in range(test_data.shape[0]):
         curr_lat = row[j][1]
         next_lon = row[j+1][0]
         next_lat = row[j+1][1]
-        
+
         har_dist_travelled = calHarDist(curr_lat,curr_lon,next_lat,next_lon)
         cum_dist += har_dist_travelled
-        
+
     dist_list.append(cum_dist)
 
 test_data['cum_dist'] = dist_list
 
-#for train 
+#for train
 dist_list = []
 
 for i in range(train_data.shape[0]):
@@ -240,10 +269,8 @@ for i in range(train_data.shape[0]):
         curr_lat = row[j][1]
         next_lon = row[j+1][0]
         next_lat = row[j+1][1]
-        
         har_dist_travelled = calHarDist(curr_lat,curr_lon,next_lat,next_lon)
         cum_dist += har_dist_travelled
-        
     dist_list.append(cum_dist)
 
 train_data['cum_dist'] = dist_list
@@ -255,36 +282,26 @@ for index, row in train_data.iterrows():
 ## Splitting origin and destination for lat and lng columns
 # For Test Data
 
-print("Performing one hot encoding")
+print("Getting Origin and Destination Lat Lng Coordinates")
 
+# for test data
 ox = []
 oy = []
 dx = []
 dy = []
 
-for i in range(count_md):
-    if test_data['ORIGIN'][i] != "":
-        origin = test_data['ORIGIN'][i]
-        ox.append(origin[0])
-        oy.append(origin[1])
-        dest = test_data['DESTINATION'][i]
-        dx.append(dest[0])
-        dy.append(dest[1])
-    else:
-        ox.append("NULL")
-        oy.append("NULL")
-        dx.append("NULL")
-        dy.append("NULL")
+for i in range(test_data.shape[0]):
+    origin = test_data['ORIGIN'][i].split(',')
+    ox.append(origin[0])
+    oy.append(origin[1])
+    dest = test_data['DESTINATION'][i].split(',')
+    dx.append(dest[0])
+    dy.append(dest[1])
 
 test_data['ORIGIN_LNG'] = ox
 test_data['ORIGIN_LAT'] = oy
 test_data['DEST_LNG'] = dx
 test_data['DEST_LAT'] = dy
-
-one_hot_call_type = one_hot(test_data,0)
-one_hot_day_type  = one_hot(test_data,1)
-test_data = test_data.join(one_hot_call_type)
-test_data = test_data.join(one_hot_day_type)
 
 # For Train Data
 
@@ -293,31 +310,33 @@ oy = []
 dx = []
 dy = []
 
-for i in range(count_md):
-    if train_data['ORIGIN'][i] != "":
-        origin = train_data['ORIGIN'][i]
-        ox.append(origin[0])
-        oy.append(origin[1])
-        dest = train_data['DESTINATION'][i]
-        dx.append(dest[0])
-        dy.append(dest[1])
-    else:
-        ox.append("NULL")
-        oy.append("NULL")
-        dx.append("NULL")
-        dy.append("NULL")
+for i in range(train_data.shape[0]):
+    origin = train_data['ORIGIN'][i].split(',')
+    ox.append(origin[0])
+    oy.append(origin[1])
+    dest = train_data['DESTINATION'][i].split(',')
+    dx.append(dest[0])
+    dy.append(dest[1])
 
 train_data['ORIGIN_LNG'] = ox
 train_data['ORIGIN_LAT'] = oy
 train_data['DEST_LNG'] = dx
 train_data['DEST_LAT'] = dy
 
+print("Adding One-hot Encoding")
+
+# for test data
+one_hot_call_type = one_hot(test_data,0)
+one_hot_day_type  = one_hot(test_data,1)
+test_data = test_data.join(one_hot_call_type)
+test_data = test_data.join(one_hot_day_type)
+
+# for train data
 one_hot_call_type = one_hot(train_data,0)
 one_hot_day_type  = one_hot(train_data,1)
 train_data = train_data.join(one_hot_call_type)
 train_data = train_data.join(one_hot_day_type)
 
-#print(train_data.head(10))
+print("Writing dataframe to CSV")
 
-train_data.to_csv('datasets/train_modified.csv')
-test_data.to_csv('datasets/test_modified.csv')
+train_data.to_csv('datasets/train_latest.csv', index=False)
