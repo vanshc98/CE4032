59fdd3285b796c022cd066ed39e1ac8fba85c325
swl3guest <bolshack_fire@hotmail.com>
2019-10-24 16:42:26 +0800

    Latest linear regression model

--- training/linear_regression.py
+++ training/linear_regression.py
@@ -4,6 +4,7 @@ import time
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
            
 DATA_DIR = '../datasets'
 
@@ -12,7 +13,7 @@ for filename in ['train_modified_v2.csv']:
     print('reading training data from %s ...' % filename)
     df = pd.read_csv(os.path.join(DATA_DIR, filename))
     y = df['DURATION']
-    df.drop(['Unnamed: 0', 'TRIP_ID', 'END_TIME', 'TIMESTAMP', 'DATE', 'DURATION', 'DEST_LNG', 'DEST_LAT'], axis=1, inplace=True)
+    df.drop(['TRIP_ID', 'END_TIME', 'TIMESTAMP', 'DATE', 'DURATION', 'DEST_LNG', 'DEST_LAT'], axis=1, inplace=True)
     values = {'ORIGIN_CALL': -1, 'ORIGIN_STAND': -1}
     df = df.fillna(value=values)
     X = np.array(df, dtype=np.float)
@@ -24,32 +25,13 @@ for filename in ['train_modified_v2.csv']:
     df = pd.read_csv(os.path.join(DATA_DIR, filename.replace('train', 'test')))
     
     ids = df['TRIP_ID']
-    df.drop(['Unnamed: 0', 'TRIP_ID', 'END_TIME', 'TIMESTAMP', 'DATE', 'CALL_TYPE_A', 'CALL_TYPE_B', 'CALL_TYPE_C', 'ACTUAL_DAYTYPE_A', 'ACTUAL_DAYTYPE_B', 'ACTUAL_DAYTYPE_C', 'DURATION', 'DEST_LNG', 'DEST_LAT'], axis=1, inplace=True)
+    df.drop(['Unnamed: 0', 'TRIP_ID', 'END_TIME', 'TIMESTAMP', 'DATE', 'DURATION', 'DEST_LNG', 'DEST_LAT'], axis=1, inplace=True)
     values = {'ORIGIN_CALL': -1, 'ORIGIN_STAND': -1}
     df = df.fillna(value=values)
     X_tst = np.array(df, dtype=np.float)
     y_pred = reg.predict(X_tst)
     
     submission = pd.DataFrame(ids, columns=['TRIP_ID'])
-    submission['TRAVEL_TIME'] = y_pred
+    submission['TRAVEL_TIME'] = (y_pred - 1) * 15
     submission.to_csv('../datasets/my_submission.csv', index=False)
-
-    # print('training a random forest regressor ...')
-    # # Initialize the famous Random Forest Regressor from scikit-learn
-    # clf = RandomForestRegressor(n_estimators=200, n_jobs=-1, random_state=21)
-    # clf.fit(X, y)
-
-    # print('predicting test data ...')
-    # df = pd.read_csv(os.path.join(DATA_DIR, filename.replace('train', 'test')))
-    # ids = df['TRIP_ID']
-    
-    # df = df.drop(['TRIP_ID', 'CALL_TYPE', 'TAXI_ID'], axis = 1)
-    # X_tst = np.array(df, dtype=np.float)
-    # y_pred = clf.predict(X_tst)
-
-    # # create submission file
-    # submission = pd.DataFrame(ids, columns=['TRIP_ID'])
-    # filename = filename.replace('train_pp', 'my_submission')
-    # submission['TRAVEL_TIME'] = np.exp(y_pred)
-    # submission.to_csv(filename, index = False)
 
